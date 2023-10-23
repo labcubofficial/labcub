97c7a20c821899b9464e144d9941058fceb06d55
godwinvimal <godwinvimal00@gmail.com>
2023-10-23 07:24:10 +0530

    Changing blog style width

--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Blog;
+use Illuminate\Support\Facades\File;
 
 class BlogController extends Controller
 {
