b6b268f96e1f0b4df2bfcf6f1b3a03360a7be0a0
godwinvimal <godwinvimal00@gmail.com>
2023-10-24 14:50:47 +0530

    final check for production

--- resources/views/front/master/landing_master.blade.php
+++ resources/views/front/master/landing_master.blade.php
@@ -16,17 +16,19 @@
 			<div class="menu-items" id="menu-items"></div>
 		</div>
 		<div class="header-label">
-			<a href="{{ url('/') }}"><h1 class="header-text">Labcub</h1><a>
+			<a href="{{ url('/') }}"><h1 class="header-text">Labcub</h1></a>
 		</div>
 		<div class="menu-btn" id="menu-btn">
-			<img class="menu-svg" src="{{ url('images/social/whatsapp.svg') }}">
+			<a target="_blank" href="https://wa.me/message/JFPMRMHADAZ6B1">
+				<img class="menu-svg" src="{{ url('images/social/whatsapp.svg') }}">
+			</a>
 		</div>
 		<div class="header-nav">
 			<ul class="header-navigation">
 				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('/') ? ' header-active' : '' }}" href="{{ url('/') }}">Home</a></li>
 				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('') ? ' header-active' : '' }}" href="#">Service</a></li>
 				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('') ? ' header-active' : '' }}" href="#">Documention</a></li>
-				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('blogs') ? ' header-active' : '' }}" href="{{ url('blogs') }}">Blog</a></li>
+				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('blogs') ? ' header-active' : '' }}" href="{{ url('blogs') }}">Blogs</a></li>
 				<li class="header-nav-item"><a class="header-nav-link{{ request()->is('legal/about_us') ? ' header-active' : '' }}" href="{{ url('legal/about_us') }}">About us</a></li>
 			</ul>
 		</div>
@@ -41,7 +43,7 @@
 
 				<ul class="social-icons">
 					<li class="footer-nav-list">
-						<a class="footer-nav-item" href="https://wa.me/message/JFPMRMHADAZ6B1" target="_blank">
+						<a class="footer-nav-item" href="https://www.facebook.com/people/Labcub/61552744947842/" target="_blank">
 							<img class="social-icon" src="{{ url('images/social/facebook.svg') }}">
 						</a>
 					</li>
@@ -68,7 +70,7 @@
 					<li class="footer-nav-list"><a class="footer-nav-item" href="{{ url('/') }}">Home</a></li>
 					<li class="footer-nav-list"><a class="footer-nav-item" href="#">Service</a></li>
 					<li class="footer-nav-list"><a class="footer-nav-item" href="#">Documention</a></li>
-					<li class="footer-nav-list"><a class="footer-nav-item" href="{{ url('blogs') }}">Blog</a></li>
+					<li class="footer-nav-list"><a class="footer-nav-item" href="{{ url('blogs') }}">Blogs</a></li>
 				</ul>
 			</div>
 			<div class="footer-nav">
@@ -78,10 +80,6 @@
 					<li class="footer-nav-list"><a class="footer-nav-item" href="{{ url('legal/terms_and_condition') }}">Terms & conditions</a></li>
 					<li class="footer-nav-list"><a class="footer-nav-item" href="{{ url('legal/about_us') }}">About us</a></li>
 				</ul>
-			</div>
-			<div class="footer-nav">
-				{{-- <h3 class="footer-links-header">Newsletter</h3> --}}
-				
 			</div>
 		</div>
 		<div class="footer-copyright">
