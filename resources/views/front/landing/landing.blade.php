3ed8b4500ac37d3c27e6720fb623a42b64097497
godwinvimal <godwinvimal00@gmail.com>
2024-03-31 18:42:38 +0530

    Hidding mobile responsive menu

--- resources/views/front/landing/landing.blade.php
+++ resources/views/front/landing/landing.blade.php
@@ -32,7 +32,7 @@
 		<div class="lg:hidden" role="dialog" aria-modal="true">
 			<!-- Background backdrop, show/hide based on slide-over state. -->
 			<div class="fixed inset-0 z-50"></div>
-			<div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+			<div style="display: none;" class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 				<div class="flex items-center justify-between">
 					<a href="#" class="-m-1.5 p-1.5">
 						<span class="sr-only">Your Company</span>
