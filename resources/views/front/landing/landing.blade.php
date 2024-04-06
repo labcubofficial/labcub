4764f0ef203f9eedad0d9246a95be61891a29dc2
godwinvimal <godwinvimal00@gmail.com>
2024-04-06 21:19:48 +0530

    changing about us page ui design

--- resources/views/front/landing/landing.blade.php
+++ resources/views/front/landing/landing.blade.php
@@ -201,7 +201,7 @@
 		<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
 			<div class="max-w-xl lg:max-w-lg">
 				<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our newsletter.</h2>
-				<p class="mt-4 text-lg leading-8 text-gray-300">Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt dolore.</p>
+				<p class="mt-4 text-lg leading-8 text-gray-300">Be the first to get exclusive offers and the latest news on our product directry in your inbox.</p>
 				<div class="mt-6 flex max-w-md gap-x-4">
 					<label for="email-address" class="sr-only">Email address</label>
 					<input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your email">
