f774b78055d1e29af2c2222a56b2b86d49be932e
godwinvimal <godwinvimal00@gmail.com>
2024-04-06 12:37:50 +0530

    Chaning footer menus link

--- resources/views/front/master/landing_master.blade.php
+++ resources/views/front/master/landing_master.blade.php
@@ -7,6 +7,7 @@
 	<!--============================== CSS Files ==============================-->
 	<link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/tailwind.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 	<!--============================== CSS Files ==============================-->
 	<!--============================== Adsense ==============================-->
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4764751757078002" crossorigin="anonymous"></script>
@@ -23,34 +24,19 @@
 		<div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
 			<nav class="flex flex-wrap justify-center -mx-5 -my-2">
 				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						About
-					</a>
+					<a href="{{ url('legal/about_us') }}" class="text-base leading-6 text-gray-500 hover:text-gray-900">About</a>
 				</div>
 				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						Blog
-					</a>
+					<a href="{{ url('blogs') }}" class="text-base leading-6 text-gray-500 hover:text-gray-900">Blog</a>
 				</div>
 				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						Team
-					</a>
+					<a href="{{ url('legal/privacy_policy') }}" class="text-base leading-6 text-gray-500 hover:text-gray-900">Policy</a>
 				</div>
 				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						Pricing
-					</a>
+					<a href="{{ url('legal/terms_and_condition') }}" class="text-base leading-6 text-gray-500 hover:text-gray-900">Terms</a>
 				</div>
 				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						Contact
-					</a>
-				</div>
-				<div class="px-5 py-2">
-					<a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-						Terms
-					</a>
+					<a href="{{ url('') }}" class="text-base leading-6 text-gray-500 hover:text-gray-900">Contact</a>
 				</div>
 			</nav>
 			<div class="flex justify-center mt-8 space-x-6">
@@ -85,7 +71,7 @@
 					</svg>
 				</a>
 			</div>
-			<p class="mt-8 text-base leading-6 text-center text-gray-400">© 2024 Labcub, Inc. All rights reserved.</p>
+			<p class="mt-8 text-base leading-6 text-center text-gray-400">© 2024 Labcub. All rights reserved.</p>
 		</div>
 	</section>
 </body>
