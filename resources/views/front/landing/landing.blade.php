2d6bc9a121a869e8409ba565bc53eaaaec4bf6d5
godwinvimal <godwinvimal00@gmail.com>
2024-05-01 18:06:10 +0530

    Changing landing page category & blog list UI design

--- resources/views/front/landing/landing.blade.php
+++ resources/views/front/landing/landing.blade.php
@@ -41,109 +41,63 @@
 	</div>
 </section>
 
-<section class="bg-white dark:bg-gray-900">
-	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-		<div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-			<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Category</h2>
-			<p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-		</div>
-		<div class="grid gap-8 lg:grid-cols-2">
+<!-- Category Start -->
+<section class="bg-white">
+	<div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+		<h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 md:text-4xl">Category</h2>
+		<div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6">
+
+			<!-- Items -->
+			<a class="w-28 h-28 text-center cursor-pointer">
+				<div class="bg-amber-100 rounded">
+					<img class="" src="{{ asset('media/category/js.png') }}" alt="">
+				</div>
+				<p class="text-base p-2">Javascript</p>
+			</a>
+			<a class="w-28 h-28 text-center cursor-pointer">
+				<div class="bg-sky-200 rounded">
+					<img class="" src="{{ asset('media/category/php.png') }}" alt="">
+				</div>
+				<p class="text-base p-2">PHP 12.2</p>
+			</a>
+			<!-- Items -->
 
 		</div>
 	</div>
 </section>
-
-
-<!-- Blog Start -->
-<div class="bg-white py-24 sm:py-32">
-	<div class="mx-auto max-w-7xl px-6 lg:px-8">
-		<div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-
-			@foreach($blogs as $blog)
-			<article class="flex max-w-xl flex-col items-start justify-between border-2 rounded">
-				<img src="{{ url('media/blog').'/'.$blog['image'] }}" alt="" class="h-full w-full">
-				<div class="flex items-center gap-x-4 text-xs pr-2 pl-2 pt-2">
-					<time datetime="2020-03-16" class="text-gray-500">{{ $blog['created_at'] }}</time>
-					<a href="{{ url('blog/').$blog['category_slug'].'/'.$blog['subcategory_name'].'/'.$blog['slug'] }}" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ $blog['category_name'] }}</a>
-				</div>
-				<div class="group relative pr-2 pl-2 pb-2">
-					<h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-						<a href="{{ url('blog').'/'.$blog['category_slug'].'/'.$blog['subcategory_name'].'/'.$blog['slug'] }}">
-							<span class="absolute inset-0"></span>{{ $blog['title'] }}</a>
-					</h3>
-					<p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{{ $blog['short_description'] }}</p>
-				</div>
-			</article>
-			@endforeach
-
-		</div>
-	</div>
-</div>
-<!-- Blog End -->
+<!-- Category End -->
 
 <!-- Blog Start -->
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-white">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 		<div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-			<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-			<p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+			<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900">Our Blog</h2>
+			<p class="font-light text-gray-500 sm:text-xl">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
 		</div>
 		<div class="grid gap-8 lg:grid-cols-2">
-			<article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+
+		@foreach($blogs as $blog)
+			<article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
 				<div class="flex justify-between items-center mb-5 text-gray-500">
-					<span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-						<svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-						</svg>
-						Tutorial
-					</span>
-					<span class="text-sm">14 days ago</span>
+					<span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">{{ $blog['category_name'] }}</span>
+					<span class="text-sm">{{ $blog['created_at'] }}</span>
 				</div>
-				<h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How to quickly deploy a static website</a></h2>
-				<p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+				<h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><a href="#">{{ $blog['title'] }}</a></h2>
+				<p class="mb-5 font-light text-gray-500">{{ $blog['short_description'] }}</p>
 				<div class="flex justify-between items-center">
 					<div class="flex items-center space-x-4">
 						<img class="w-7 h-7 rounded-full" src="" alt="Jese Leos avatar" />
-						<span class="font-medium dark:text-white">
-							Jese Leos
-						</span>
+						<span class="font-medium">Labcub</span>
 					</div>
-					<a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-						Read more
+					<a href="#" class="inline-flex items-center font-medium text-primary-600 hover:underline">Read more
 						<svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
 						</svg>
 					</a>
 				</div>
 			</article>
-			<article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-				<div class="flex justify-between items-center mb-5 text-gray-500">
-					<span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-						<svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path>
-							<path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-						</svg>
-						Article
-					</span>
-					<span class="text-sm">14 days ago</span>
-				</div>
-				<h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Our first project with React</a></h2>
-				<p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-				<div class="flex justify-between items-center">
-					<div class="flex items-center space-x-4">
-						<img class="w-7 h-7 rounded-full" src="" alt="Bonnie Green avatar" />
-						<span class="font-medium dark:text-white">
-							Bonnie Green
-						</span>
-					</div>
-					<a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-						Read more
-						<svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-						</svg>
-					</a>
-				</div>
-			</article>
+		@endforeach
+
 		</div>
 	</div>
 </section>
