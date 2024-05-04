e288bdcb9a973d727a79bff62d1834cdb9572ade
godwinvimal <godwinvimal00@gmail.com>
2024-05-04 11:47:23 +0530

    Blog read link implemented

--- resources/views/front/landing/landing.blade.php
+++ resources/views/front/landing/landing.blade.php
@@ -65,30 +65,21 @@
 <section class="bg-white">
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 		<div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-			<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900">Our Blog</h2>
-			<p class="font-light text-gray-500 sm:text-xl">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+			<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900">Blogs</h2>
 		</div>
-		<div class="grid gap-8 lg:grid-cols-2">
+		<div class="grid gap-8 lg:grid-cols-3">
 
 		@foreach($blogs as $blog)
-			<article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
-				<div class="flex justify-between items-center mb-5 text-gray-500">
-					<span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">{{ $blog['category_name'] }}</span>
+			<article class="bg-white rounded border border-gray-200 shadow-md">
+				<a href="{{ url('blog').'/'.$blog['category_name'].'/'.$blog['subcategory_name'].'/'.$blog['slug'] }}">
+					<img class="mb-5 rounded-t" src="{{ url('media/blog').'/'.$blog['image'] }}" alt="">
+				</a>
+				<div class="pl-4 pr-4 flex justify-between items-center mb-5 text-gray-500">
+					<span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center rounded">{{ $blog['category_name'] }}</span>
 					<span class="text-sm">{{ $blog['created_at'] }}</span>
 				</div>
-				<h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><a href="#">{{ $blog['title'] }}</a></h2>
-				<p class="mb-5 font-light text-gray-500">{{ $blog['short_description'] }}</p>
-				<div class="flex justify-between items-center">
-					<div class="flex items-center space-x-4">
-						<img class="w-7 h-7 rounded-full" src="" alt="Jese Leos avatar" />
-						<span class="font-medium">Labcub</span>
-					</div>
-					<a href="#" class="inline-flex items-center font-medium text-primary-600 hover:underline">Read more
-						<svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-						</svg>
-					</a>
-				</div>
+				<h2 class="pl-4 pr-4 mb-2 text-xl font-bold tracking-tight text-gray-900"><a href="{{ url('blog').'/'.$blog['category_name'].'/'.$blog['subcategory_name'].'/'.$blog['slug'] }}">{{ $blog['title'] }}</a></h2>
+				<p class="pl-4 pr-4 mb-5 text-sm font-light text-gray-500">{{ $blog['short_description'] }}</p>
 			</article>
 		@endforeach
 
